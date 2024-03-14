27a5ab850c2a4123048b1041925fcbbe6cfa2235
Xonjamil <xonjamil@gmail.com>
2024-03-14 19:19:49 +0500

    game

---
+++ js.js/script.js
@@ -0,0 +1,21 @@
+const username = "Yura"
+
+let bets = document.querySelectorAll('.bet')
+bets.forEach((btn) => {
+    btn.addEventListener('click', betSelector)
+    btn.setAttribute('data-sum', btn.innerHTML)
+})
+ 
+function betSelector() {
+
+    bets.forEach((btn) => {
+    btn.classList.remove('selected')
+    })
+    
+    let currentBtn = event.target
+    currentBtn.classList.add('selected')
+
+    betsum = currentBtn.getAttribute('data-sum')
+    console.log(betsum)
+}
+
